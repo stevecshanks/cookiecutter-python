1d9abb506c3582af2028f418e75dce463ffd9289
Steve Shanks <steve.c.shanks@gmail.com>
2017-06-25 09:06:17 +0100

    Update hook to initialise Git repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,23 @@
 import venv
+import subprocess
 
 
-print("Creating virtual environment...")
-venv.create('venv', with_pip=True)
+def main():
+    print("Creating virtual environment...")
+    create_virtual_environment()
+    print("Setting up Git repository...")
+    create_git_repository()
+    print("Done!")
+
+
+def create_virtual_environment():
+    venv.create('venv', with_pip=True)
+
+
+def create_git_repository():
+    subprocess.run(['git', 'init'])
+    subprocess.run(['git', 'add', '*'])
+    subprocess.run(['git', 'commit', '-m', 'Set up Cookiecutter repository'])
+
+
+main()
